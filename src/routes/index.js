64d3ae07d4cd646a6beb4a40bf5dd0be937798dc
Andrewij21 <andrewijaya988@gmail.com>
2023-06-22 22:33:25 +0700

    [FEAT] add morgan log

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,15 @@
 const route = require("express").Router();
+const morgan = require("morgan");
 const studentRoutes = require("./studentRoutes");
 const authRoutes = require("./authRoutes");
 const ORIGIN_URI = "/api/v1";
+
+// Use LOG request
+route.use(morgan("dev"));
+// route.use((req, res, next) => {
+//   console.info(req.method, req.url);
+//   next();
+// });
 
 route.use(ORIGIN_URI + "/student", studentRoutes);
 route.use(ORIGIN_URI + "/auth", authRoutes);
