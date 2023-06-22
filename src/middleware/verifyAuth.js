6242b81d259eb186a56f47f311409c889b0c0132
Andrewij21 <andrewijaya988@gmail.com>
2023-06-22 22:34:08 +0700

    [REFACTOR] devide verify

--- src/middleware/verifyAuth.js
+++ src/middleware/verifyAuth.js
@@ -1,18 +1,37 @@
 const jwt = require("jsonwebtoken");
 const Student = require("../models/studentModel");
 
-const verifyAuth = async (req, res, next) => {
-  const authorization = req.headers.authorization;
+// const verifyAuth = async (req, res, next) => {
+//   const authorization = req.headers.authorization;
+//   const token = authorization && authorization.split(" ")[1];
+//   if (!token) return res.status(401).json({ error: "Null Token" });
+
+//   try {
+//     const { _id } = jwt.verify(token, process.env.SECRET_TOKEN);
+//     req.user_id = await Student.findById(_id).select("_id");
+//     next();
+//   } catch (error) {
+//     res.status(403).json({ error: "Invalid Token" });
+//   }
+// };
+
+function checkToken(authorization, res) {
+  // const authorization = req.headers.authorization;
   const token = authorization && authorization.split(" ")[1];
   if (!token) return res.status(401).json({ error: "Null Token" });
-
-  try {
-    const { _id } = jwt.verify(token, process.env.SECRET_TOKEN);
-    req.user_id = await Student.findById(_id).select("_id");
-    next();
-  } catch (error) {
-    res.status(403).json({ error: "Invalid Token" });
+  return token;
+}
+class VerifyAuth {
+  async verifyAuthStudent(req, res, next) {
+    const token = checkToken(req.headers.authorization, res);
+    if (typeof token === "object") return;
+    try {
+      const { _id } = jwt.verify(token, process.env.SECRET_TOKEN);
+      req.user_id = await Student.findById(_id).select("_id");
+      next();
+    } catch (error) {
+      res.status(403).json({ error: "Invalid Token" });
+    }
   }
-};
-
-module.exports = verifyAuth;
+}
+module.exports = new VerifyAuth();
--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -1,9 +1,10 @@
 const route = require("express").Router();
 const studentController = require("../controller/student_controller");
-const verify = require("../middleware/verifyAuth");
+const verify = require("../middleware/verifyAuth.js");
 
 // const { signupCheck, loginCheck } = require("../middleware/validator");
-route.use(verify);
+// route.use((req, res, next) => verify.verifyAuthStudent(req, res, next));
+route.use(verify.verifyAuthStudent);
 
 route.get("/", studentController.students);
 route.delete("/:id", studentController.removeStudent);
