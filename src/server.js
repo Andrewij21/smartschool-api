bfaab5444914c176e21b6caf576aaad26266dd77
Andrewij21 <andrewijaya988@gmail.com>
2023-06-22 11:17:07 +0700

    [FEAT] Add helmet and compression

--- src/server.js
+++ src/server.js
@@ -2,24 +2,30 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
+const helmet = require("helmet");
+// const compression = require("compression");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes");
 
 // ORIGIN
 // const corsOptions = { origin: "https://poetic-halva-a0d5af.netlify.app" };
-app.use(cors());
-
-// POST json data
-app.use(express.json());
+app.use(cors()); //Handle premission
+app.use(express.json()); // POST json data
+app.use(helmet()); // Secure HTTP headers
+// app.use(
+//   compression({
+//     level: 6,
+//     threshold: 0,
+//   })
+// ); //Compress response
+app.use(routes); // Handle all routes
 
 // Use LOG request
 app.use((req, res, next) => {
   console.info(req.method, req.url);
   next();
 });
-
-app.use(routes);
 
 mongoose.set("strictQuery", false);
 mongoose
