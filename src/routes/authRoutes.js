26b7042f93bd988a8c8c86daaf3f464053e9b598
Andrewij21 <andrewijaya988@gmail.com>
2023-06-24 09:35:22 +0700

    [REFACTOR] remove unesecerry ROLE

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,6 @@
 const route = require("express").Router();
 const authController = require("../controller/auth_controller");
-const ROLE = {
-  student: "student",
-  parent: "parent",
-  teacher: "teacher",
-};
 
-// student
 route.post(`/:role/login`, authController.login);
 route.post(`/:role/register`, authController.register);
 
