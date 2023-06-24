a2847573869b66a6b9bc7db46ee5c16cbdc48a20
Andrewij21 <andrewijaya988@gmail.com>
2023-06-24 09:23:00 +0700

    [REFACTOR] simplyfy logic

--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -1,22 +1,15 @@
 const route = require("express").Router();
 const studentController = require("../controller/student_controller");
 const verify = require("../middleware/verifyAuth.js");
+const checkRolesAccess = require("../utils/checkRolesAccess");
+const {
+  ROLES: { admin, teacher, parent, student },
+} = require("../utils/roles");
 
 // const { signupCheck, loginCheck } = require("../middleware/validator");
-// route.use((req, res, next) => verify.verifyAuthStudent(req, res, next));
 route.use(verify.verifyAuth);
 
-function checkRolesAccess(roles) {
-  return (req, res, next) => {
-    verify.verifyAuthRole(req, res, next, roles);
-  };
-}
-
-route.get(
-  "/",
-  checkRolesAccess(["admin", "teacher"]),
-  studentController.students
-);
+route.get("/", checkRolesAccess([admin]), studentController.students);
 route.delete("/:id", studentController.removeStudent);
 route.patch("/", studentController.updateStudent);
 
