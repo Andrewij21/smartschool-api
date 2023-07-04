c357701083a39c8b5987f633e8d20d88cb3fa920
Andrewij21 <andrewijaya988@gmail.com>
2023-07-04 14:50:26 +0700

    [FEAT] add class route and fix log info

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,15 @@ const studentRoutes = require("./studentRoutes");
 const teacherRoutes = require("./teacherRoutes");
 const authRoutes = require("./authRoutes");
 const subjectRoutes = require("./subjectRoutes");
+const classRoutes = require("./classRoutes");
 const ORIGIN_URI = "/api/v1";
 
 // Use LOG request
-route.use(morgan("dev"));
+route.use(
+  morgan(
+    ":date[Asia/Jakarta] :method :url :status :response-time ms - :res[content-length]"
+  )
+);
 // route.use((req, res, next) => {
 //   console.info(req.method, req.url);
 //   next();
@@ -17,5 +22,6 @@ route.use(ORIGIN_URI + "/student", studentRoutes);
 route.use(ORIGIN_URI + "/teacher", teacherRoutes);
 route.use(ORIGIN_URI + "/auth", authRoutes);
 route.use(ORIGIN_URI + "/subject", subjectRoutes);
+route.use(ORIGIN_URI + "/class", classRoutes);
 
 module.exports = route;
