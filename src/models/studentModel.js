66ce8cec2243aff009e2df8e49c2b8cdc29f1901
Andrewij21 <andrewijaya988@gmail.com>
2023-07-10 14:02:00 +0700

    [REFACTOR] update student model

--- src/models/studentModel.js
+++ src/models/studentModel.js
@@ -6,10 +6,22 @@ const schema = new Schema(
       type: String,
       required: true,
     },
+    email: {
+      type: String,
+      required: true,
+    },
     nis: {
       type: String,
       required: true,
       unique: true,
+    },
+    nisn: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    school_origin: {
+      type: String,
     },
     role: {
       type: String,
@@ -21,8 +33,11 @@ const schema = new Schema(
       type: String,
       required: true,
     },
-    birthDate: {
+    birth_date: {
       type: Date,
+    },
+    birth_place: {
+      type: String,
     },
     class: {
       type: Schema.Types.ObjectId,
@@ -37,13 +52,69 @@ const schema = new Schema(
     address: {
       type: String,
     },
+    religion: {
+      type: String,
+    },
+    phone_number: {
+      type: String,
+    },
     active: {
       type: Boolean,
       required: true,
     },
+
+    // BAPAK
+    father_data: {
+      nik: String,
+      name: String,
+      birthDate: String,
+      religion: String,
+      last_education: String,
+      job: String,
+      income: String,
+      birthPlace: String,
+      citizenship: String,
+      phone_number: {
+        type: String,
+        default: "-",
+      },
+    },
+    // EMAK
+    mother_data: {
+      nik: String,
+      name: String,
+      birthDate: String,
+      religion: String,
+      last_education: String,
+      job: String,
+      income: String,
+      birthPlace: String,
+      citizenship: String,
+      phone_number: {
+        type: String,
+        default: "-",
+      },
+    },
+    // WALI
+    guardian_data: {
+      nik: String,
+      name: String,
+      birthDate: String,
+      religion: String,
+      last_education: String,
+      job: String,
+      income: String,
+      birthPlace: String,
+      citizenship: String,
+      phone_number: {
+        type: String,
+        default: "-",
+      },
+    },
   },
   {
     timestamps: true,
+    versionKey: false,
   }
 );
 // const schema = new Schema(
